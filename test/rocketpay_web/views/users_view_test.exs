1b43120b7d935b1dee7119b80846574718f3f971
Luan Farias <luan.farias_bvs@outlook.com>
2021-03-02 01:45:28 -0300

    Finished app

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,36 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Rocketpay.{User, Account}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Luan",
+      password: "123456",
+      nickname: "lunsmat",
+      email: "luan@farias.com",
+      age: 27
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} = Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Luan",
+        nickname: "lunsmat"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
